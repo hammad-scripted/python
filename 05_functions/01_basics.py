bd8885fc254bd57f8394500e6ae6bb979ecd5f29
Mohammad Hammad <hammad.scripted@gmail.com>
2026-09-13 00:58:17 +0530

    args and kwargs

--- 05_functions/01_basics.py
+++ 05_functions/01_basics.py
@@ -51,3 +51,42 @@ introduce(age=30, city="London", name="Bob")
 
 # Mixing positional and keyword arguments
 # If you mix positional and keyword arguments, the keyword arguments will override the positional arguments and generally positional should come first otherwise it will throw an error
+
+# multiple return values
+
+def get_name_and_age():
+    return "Alice", 25
+
+
+name, age = get_name_and_age()
+print(name)
+print(age)
+
+
+# args and kwargs
+# args accept any number of positional arguments
+
+def print_numbers(*args):
+    for num in args:
+        print(num)
+
+print_numbers(1, 2, 3, 4, 5)
+
+
+def add_all(*args):
+    return sum(args)
+
+
+result = add_all(1, 2, 3, 4, 5)
+print(result)
+
+
+# kwargs accept any number of keyword arguments
+
+def print_info(**details):
+    # details is a dictionary of key-value pairs
+    print(details)
+    for key,value in details.items():
+        print(f"{key}: {value}")
+
+print_info(name="Alice", age=25, city="New York")
