076d66307a2ee1dcc334fb0046f2af06f25d3584
Mohammad Hammad <hammad.scripted@gmail.com>
2026-09-13 17:56:46 +0530

    import export

---
+++ 05_functions/import.py
@@ -0,0 +1,19 @@
+import os
+import math as m
+
+print(m.pi)
+
+print(os.getcwd())
+
+
+from math import pi
+
+print(pi)
+
+from math import pi as p
+
+print(p)
+
+
+from datetime import datetime
+print(datetime.now())
