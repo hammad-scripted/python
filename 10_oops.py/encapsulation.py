fd2536f1cc16d981924960d8d5b6ff584099ba43
Mohammad Hammad <hammad.scripted@gmail.com>
2026-09-21 23:35:33 +0530

    getters and setters

--- 10_oops.py/encapsulation.py
+++ 10_oops.py/encapsulation.py
@@ -1,8 +1,8 @@
 class BankAccount:
     def __init__(self,owner,balance):
-        self.owner=owner
-        self._pin="1234"
-        self.__account_number="123456789"
+        self.owner=owner # public
+        self._pin="1234" #  protected
+        self.__account_number="123456789" #private
         self.balance=balance
         
     def withdraw(self,amount):
@@ -16,6 +16,22 @@ class BankAccount:
 account = BankAccount("Alex", 1000)
 print(account.owner)  
 print(account._pin)
-print(account._BankAccount__account_number)
+print(account._BankAccount__account_number) # way to call private variable
 print(account.withdraw(200))
 print(account.deposit(500))
+
+
+class Employee:
+    def __init__(self,name,salary):
+        self.name=name
+        self.__salary=salary
+    def get_salary(self):
+        return self.__salary
+    def set_salary(self,salary):
+        self.__salary=salary
+    
+        
+employee=Employee("Alex",50000)
+print(employee.get_salary())
+employee.set_salary(60000)
+print(employee.get_salary())
