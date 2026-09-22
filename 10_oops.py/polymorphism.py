dce05e7b1291e8c850a9dc0998bc06084e4c7e43
Mohammad Hammad <hammad.scripted@gmail.com>
2026-09-22 21:35:22 +0530

    add method overloading example with Calculator class

--- 10_oops.py/polymorphism.py
+++ 10_oops.py/polymorphism.py
@@ -16,3 +16,23 @@ def make_animal_speak(animal_object):
 
 make_animal_speak(Dog())  # Output: Woof!
 make_animal_speak(Cat())  # Output: Meow!
+make_animal_speak(Animal())  # Output: Generic animal sound
+
+#method overloading
+
+print("method overloading")
+
+# example
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def add(self, a, b, c):
+        return a + b + c
+    
+
+calc = Calculator()
+print(calc.add(1, 2))  # Output: 3
+print(calc.add(1, 2, 3))  # Output: 6       
+
